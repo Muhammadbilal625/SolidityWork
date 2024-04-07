430467c3112765fc02b6ec52e9da97e4af45ed67
Muhammadbilal625 <93539883+Muhammadbilal625@users.noreply.github.com>
2024-04-07 16:48:06 +0500

    Update and rename abiEncode.sol to abiEncodeAndDecode.sol

--- abiEncodeAndDecode.sol
+++ abiEncodeAndDecode.sol
@@ -35,3 +35,35 @@ contract AbiEncode{
         return abi.encodeCall(IERC20.transfer,(to,amount));
     }
 }
+
+
+contract AbiDecode {
+    struct MyStruct {
+        string name;
+        uint256[2] nums;
+    }
+
+    function encode(
+        uint256 x,
+        address addr,
+        uint256[] calldata arr,
+        MyStruct calldata myStruct
+    ) external pure returns (bytes memory) {
+        return abi.encode(x, addr, arr, myStruct);
+    }
+
+    function decode(bytes calldata data)
+        external
+        pure
+        returns (
+            uint256 x,
+            address addr,
+            uint256[] memory arr,
+            MyStruct memory myStruct
+        )
+    {
+        // (uint x, address addr, uint[] memory arr, MyStruct myStruct) = ...
+        (x, addr, arr, myStruct) =
+            abi.decode(data, (uint256, address, uint256[], MyStruct));
+    }
+}
